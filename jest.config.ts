45138cb192c30bbe508f088a921e67f2cd33c51d
Luan Marques <luanfmarques10@gmail.com>
2022-10-29 06:40:01 -0300

    add jest

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/coverage/**",
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
